0281e2984aa57c0eb7fe59efc2a877e7e545aef8
nngt886 <a319166880@gmail.com>
2026-03-15 21:47:01 +0800

    Update collects.py

--- collects.py
+++ collects.py
@@ -87,7 +87,7 @@ for url, shortname in sources.items():
 
 # 写入 ipv4.txt（仅IPv4）
 with open('ipv4.txt', 'w') as f4:
-    f4.write(f"ipv4.list.updated.at#Upd{timestamp}\n")
+#    f4.write(f"ipv4.list.updated.at#Upd{timestamp}\n")
     for ip in sorted(ipv4_dict):
         f4.write(f"{ip}#{ipv4_dict[ip]}\n")
 
